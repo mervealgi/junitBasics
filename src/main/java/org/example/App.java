9e97e88ebcd7e1017802b6160c42ccd7f50afa8f
mervealgi <72434705+mervealgi@users.noreply.github.com>
2021-04-17 00:44:23 +0300

    update v2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,5 @@
 package org.example;
-
+import java.util.Locale;
 
 public class App
 {
@@ -11,12 +11,12 @@ public class App
         return true;
     }
 
-    public int sum(int i1 , int i2){
-
-        return i1 + i2;
+    public int sum(int a1 , int a2){
+        return a1 + a2;
     }
 
-
-
-
+    public String toLowerCase(String lowerCaseChar){
+        return lowerCaseChar.toLowerCase(Locale.forLanguageTag("TR"));   //usage for my language
+        //return lowerCaseChar.toLowerCase(Locale.ENGLISH);              //usage for international
+    }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,26 +1,73 @@
 package org.example;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import org.junit.*;
+import org.junit.rules.Timeout;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
 
 
 public class AppTest {
-    App app = new App();
 
-    @Test
+    App app = new App();
+    Object object;
+
+    @BeforeClass                                                          //Execute only once so shouldnt take place on memory : static
+    public static void beforeClass(){
+        System.out.println("Before Class");
+    }
+
+    @Before
+    public void beforeMethod() {
+        System.out.println("Test Started");
+    }
+    
+    //@Rule
+    //public Timeout globalTimeout = Timeout.seconds(20);
+
+    @Test //(timeout = 1000)                                            Test has to finish in 1000ms
     public void shouldAnswerWithTrue() {
-        assertEquals("Verilen ifadeler uyuştu", "Hello World", app.helloWorld());
+        assertEquals("Values arent equal", "Hello World", app.helloWorld());
     }
 
     @Test
     public void checkVisible() {
-        assertTrue("True değer gelmedi" , app.checkVisible());
+        assertTrue("True wasnt return", app.checkVisible());
     }
 
     @Test
-    public void sumTest(){
-        assertEquals("Toplama işlemi hatalı", 2 , app.sum(-3,5));
+    public void sumTest() {
+        assertEquals("Incorrect operation", 2, app.sum(-3, 5));
+    }
+
+    @Test
+    public void lowerCaseTest() {
+        assertEquals("Typographical error", "algı", app.toLowerCase("ALGI"));
+    }
+
+    @Test
+    public void nullTest() {
+        assertNull("Object is not null", object);
+        //assertNotNull( "Object is not null", object);                 //Test gonna be failed
+    }
+
+    @Test
+    public void sameControlTest() {
+        int num1 = 5;
+        int num2 = 5;                                                   //Not gonna be the same even the variable types ld be long/float/double/byte.
+        Integer num3 = 5;
+        int summ;
+
+        assertSame("Objects are not same", num1, num2);
+        assertSame("Objects are not same", num1, num3);
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("End of the test.");
+    }
+
+    @AfterClass
+    public static void afterClass(){                                    //Execute only once so shouldnt take place on memory
+        System.out.println("After Class");
     }
 
 
